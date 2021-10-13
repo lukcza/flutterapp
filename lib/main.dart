4b2085525fd6500117b775d7942e142906e78362
lukcza <lukcza23@gmail.com>
2021-10-13 21:37:29 +0200

    First routes step

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+      title: 'Named Routes ',
+      initialRoute: '/',
+      routes: {
+          '/': (context) => const MyApp(),
+      }
+   ),
+  );
 }
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -13,43 +20,49 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body:Container(
-        alignment: Alignment.center,
-        color: const Color.fromARGB(0,241, 232, 184,),
-        child:Column(
-          children: [
-            Flexible(
-              child:Container(
-                child:Row(
-                  children: <Widget>[
-                    TextField(),
-                  ],
+    return MaterialApp(
+        // routes:const {
+        //     '/page2':() =>
+        // },
+        home: Scaffold(
+
+        resizeToAvoidBottomInset: false,
+        body:Container(
+            alignment: Alignment.center,
+            color: const Color.fromARGB(0,241, 232, 184,),
+            child:Column(
+            children: [
+                Flexible(
+                child:Container(
+                    child:Row(
+                    children: <Widget>[
+                        TextField(),
+                    ],
+                    ),
                 ),
-              ),
-            ),
-            Flexible(
-              child:Container(
-                child:Row(
-                  children: <Widget>[
-                    TextField(),
-                  ],
                 ),
-              ),
-            ),
-            Flexible(
-              child:Container(
-                child:Row(
-                  children: <Widget>[
-                    ElevatedButton(onPressed: q, child: child),
-                  ],
+                Flexible(
+                child:Container(
+                    child:Row(
+                    children: <Widget>[
+                        TextField(),
+                    ],
+                    ),
                 ),
-              ),
+                ),
+                Flexible(
+                child:Container(
+                    child:Row(
+                    children: <Widget>[
+                        ElevatedButton(onPressed: q, child: child),
+                    ],
+                    ),
+                ),
+                ),
+            ],
             ),
-          ],
         ),
-      ),
-    );
+        ),
+  );
   }
 }
